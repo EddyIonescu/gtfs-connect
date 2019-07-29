c3143281696c4fcf5f234d002baa8d5eb741be3a
eddyionescu <eddyionescu@gmail.com>
2019-07-29 11:04:34 -0400

    update trip connections script

--- trip_connections.py
+++ trip_connections.py
@@ -1,16 +1,14 @@
 import csv
+import json
 import numpy as np
 import partridge as ptg
 import pandas as pd
+import numpy as np
 import os
 import xlsxwriter
 
 
-from haversine import haversine
-
 # This script gets all MSP arrivals and departures that occur near a GO station, organized by Stop ID.
-
-
 
 inpaths = [
     'GO',
@@ -41,8 +39,6 @@ def get_feed_df(inpath):
 
 def add_agency_col(df, agency, id_fields):
     df['agency'] = agency
-    #for id_field in id_fields:
-    #    df[id_field] = df[id_field].apply(lambda id : agency + ' ' + id)
     return df
 
 def fill_in_stop_times(df):
@@ -59,7 +55,7 @@ def get_agency_short_name(feed_df):
     return agency_short_name
 
 def initialize_feeds():
-    feed_dfs = [get_feed_df('gtfs/'+inpath+'.zip') for inpath in inpaths]
+    feed_dfs = [get_feed_df('gtfs_winter_2019/'+inpath+'.zip') for inpath in inpaths]
     global stops_df
     stops_df = pd.concat([
         add_agency_col(
@@ -106,25 +102,38 @@ def initialize_feeds():
     print(stop_times_df.head())
     print(routes_df.head())
 
-def read_input_file(inputreader):
-    # station names and route IDs agnostic among agencies
-    station_names = next(inputreader)[1:]
-    corridor_route_ids = next(inputreader)[1:]
-    connection_max_distance = int(next(inputreader)[1])
-    min_inbound_minutes = int(next(inputreader)[1])
-    max_inbound_minutes = int(next(inputreader)[1])
-    min_outbound_minutes = int(next(inputreader)[1])
-    max_outbound_minutes = int(next(inputreader)[1])
-    return {
-        'station_names': station_names,
-        'corridor_route_ids': corridor_route_ids,
-        'connection_max_distance': connection_max_distance,
-        'min_inbound_minutes': min_inbound_minutes,
-        'max_inbound_minutes': max_inbound_minutes,
-        'min_outbound_minutes': min_outbound_minutes,
-        'max_outbound_minutes': max_outbound_minutes,
-    }
 
+# returns map of station -> list of values
+def read_stations_config_csv(path):
+    with open(path, encoding='utf-8-sig') as stations_csv:
+        stations_reader = csv.reader(stations_csv, delimiter=',')
+        return {
+            station_row[0]: station_row[1:]
+            for station_row in stations_reader
+        }
+
+
+def read_stations(input_path):
+   return read_stations_config_csv(input_path+'/Stations.csv')
+
+
+def read_location_overrides(input_path):
+    return read_stations_config_csv(input_path+'/Locations.csv')
+
+
+def read_config():
+    with open('./config.json') as config_file:
+        return json.load(config_file)
+
+def haversine(lat1, lon1, lat2, lon2):
+    MILES = 3959
+    lat1, lon1, lat2, lon2 = map(np.deg2rad, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1 
+    dlon = lon2 - lon1 
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    total_miles = MILES * c
+    return total_miles * 1.6 * 1000 # return metres
 
 # now turn it into the readable format with inbound/outbound/both fields
 # result also used as a unique ID for arrivals returned
@@ -132,10 +141,15 @@ def get_stop_time_route_stop(row):
     trip_name = str(row['trip_headsign']) or str(row['trip_short_name'])
     # always needed for new rows format
     route_id = str(row['route_short_name']) # + ' ' if str(row['route_short_name']) not in trip_name else ''
-    # place spaces since these parts could be empty strings - strip leading/trailing whitespace when outputting to workbook
+    # token inserted as to properly split when outputting
     return str(row['agency']) + ' &gtfstoken& ' + str(route_id) + ' &gtfstoken& '  + trip_name + ' at ' + str(row['stop_name'])
 
+# returns hour (hh:00) of time string (hh:mm)
+# used in-place of corridor to more easily view and compare trips on an hour-by-hour basis
+def get_hour_of_time(time_hhmm):
+    return time_hhmm[:2] + ':00'
 
+# whether a stop arrival belongs to a Corridor route
 def is_corridor_stop_time(stop_time, station_stops, corridor_route_ids):
     contains_stop = False
     for station_stop in station_stops.itertuples(index=False):
@@ -147,11 +161,6 @@ def is_corridor_stop_time(stop_time, station_stops, corridor_route_ids):
         return True
     return False
 
-# returns hour (hh:00) of time string (hh:mm)
-# used in-place of corridor to more easily view and compare trips on an hour-by-hour basis
-def get_hour_of_time(time_hhmm):
-    return time_hhmm[:2] + ':00'
-
 
 def get_stop_time_meeting_types(
     nearby_stop_times_df,
@@ -162,6 +171,7 @@ def get_stop_time_meeting_types(
     max_inbound_minutes,
     min_outbound_minutes,
     max_outbound_minutes,
+    hourly_summary,
 ):
     """Using the supplied inbound/outbound transfer limits, determines whether each transit trip
     corresponds to a Corridor trip, an Inbound connection (to a corridor), an Outbound connection
@@ -175,8 +185,10 @@ def get_stop_time_meeting_types(
         nonlocal recentmost_corridor_arrival_time
         if is_corridor_stop_time(row, station_stops, corridor_route_ids): 
             recentmost_corridor_arrival_time = row['arrival_time']
+            if hourly_summary:
+                row[get_stop_time_route_stop(row)] = get_hour_of_time(row['arrival_time_hhmm'])
+                return row
             row[get_stop_time_route_stop(row)] = 'Corridor'
-            # get_hour_of_time(row['arrival_time_hhmm']) - used in rail corridor task
         outbound_transfer_min = (row['departure_time'] - recentmost_corridor_arrival_time) // 60
         inbound_transfer_min = (recentmost_corridor_arrival_time - row['departure_time']) // 60
         if direction == 'outbound' and outbound_transfer_min >= min_outbound_minutes and outbound_transfer_min <= max_outbound_minutes:
@@ -204,26 +216,36 @@ def get_local_msp_connections(
     max_inbound_minutes,
     min_outbound_minutes,
     max_outbound_minutes,
-    only_show_corridors=False,
+    only_show_corridors,
+    hourly_summary,
+    location_overrides,
 ):
-    # corridor_stop_ids = ['GO GO', 'GO 02629'] # all stops matching station name
     station_stops = stops_df.loc[stops_df['stop_name'] == station_name]
-    print(station_stops)
     if len(station_stops) == 0:
         # station doesn't exist, return empty
         return ([], [], station_name)
 
-    # TODO - optimize this part, takes ages, everythins else is fast
-    stops_df['connection_distance'] = stops_df.apply(
-        # use minimum distance to any stop matching the station name
-        lambda stop_df : min([haversine(
-            lon1=stop_df['stop_lon'],
-            lat1=stop_df['stop_lat'],
-            lon2=station_stop.stop_lon,
-            lat2=station_stop.stop_lat,
-        ) for station_stop in station_stops.itertuples()]),
-        axis=1,
-    )
+    if len(location_overrides) > 0:
+        new_station_stops = []
+        station_stops = station_stops.to_dict(orient='records')
+        for station_stop in station_stops:
+            for location in location_overrides:
+                new_station_stop = dict(station_stop)
+                new_station_stop['stop_lat'] = float(location.split(',')[0])
+                new_station_stop['stop_lon'] = float(location.split(',')[1])
+                new_station_stops.append(new_station_stop)
+        station_stops = pd.DataFrame(new_station_stops)
+
+    # gets connection distance between each station and all stops
+    connections = []
+    for station_stop in station_stops.itertuples():
+       connections.append(haversine(
+           station_stop.stop_lat,
+           station_stop.stop_lon,
+           stops_df['stop_lat'],
+           stops_df['stop_lon'],
+        ))
+    stops_df['connection_distance'] = pd.concat(connections).groupby(level=0).min()
     nearby_stops_df = stops_df[(stops_df['connection_distance'] <= connection_max_distance) | (stops_df['stop_name'] == station_name)]
     print('Nearby Stops: ', nearby_stops_df.head())
 
@@ -260,7 +282,6 @@ def get_local_msp_connections(
         validate='many_to_one',
     )
     nearby_stop_times_df.reset_index(inplace=True)
-
     print(nearby_stop_times_df.head())
 
     # convert times to readable format
@@ -271,7 +292,6 @@ def get_local_msp_connections(
     nearby_stop_times_df['departure_time_hhmm'] = nearby_stop_times_df['departure_time'].apply(seconds_to_clocktime)
     nearby_stop_times_df = nearby_stop_times_df.sort_values(['arrival_time_hhmm', 'departure_time_hhmm'])
     print('Transit at Station:', nearby_stop_times_df)
-
     # output dev file
     nearby_stop_times_df.to_csv(
         './output/dev/{station_name}-raw.csv'.format(
@@ -279,7 +299,6 @@ def get_local_msp_connections(
         ),
         index=False,
     )
-
     # identify whether arrivals/departures are inbound/outbound/both/none
     nearby_stop_times_df = get_stop_time_meeting_types(
         nearby_stop_times_df,
@@ -290,6 +309,7 @@ def get_local_msp_connections(
         max_inbound_minutes,
         min_outbound_minutes,
         max_outbound_minutes,
+        hourly_summary,
     )
     nearby_stop_times_df = get_stop_time_meeting_types(
         nearby_stop_times_df[::-1],
@@ -300,6 +320,7 @@ def get_local_msp_connections(
         max_inbound_minutes,
         min_outbound_minutes,
         max_outbound_minutes,
+        hourly_summary,
     )
     nearby_stop_times_df = nearby_stop_times_df[::-1]
     if only_show_corridors:
@@ -332,7 +353,6 @@ def get_local_msp_connections(
         # unique errors on empty dataframes, which is possible for non-corridors
         route_non_corridor_stops = sorted(route_non_corridor_stops_df.unique())
     route_stops += route_non_corridor_stops
-
     # return final file here
     header = ['Arrival Time', 'Departure Time', *route_stops]
     connection_dicts = nearby_stop_times_df.apply(
@@ -345,49 +365,16 @@ def get_local_msp_connections(
     )
     return (header, connection_dicts, station_name)
 
-# DEPRECATED - TODO - REMOVE
-# each column is a route/pattern/stop, each row is an arrival/departure
-def output_workbook(workbook_name, connections):
-    workbook = xlsxwriter.Workbook('./output/'+workbook_name+'.xlsx')
-    cell_format = workbook.add_format()
-    cell_format.set_text_wrap()
-    for (headers, connection_dicts, station_name) in connections:
-        worksheet = workbook.add_worksheet(name=station_name)
-        worksheet.autofilter(0, 0, 0, len(headers)-1)
-        for i in range(len(headers)):
-            worksheet.write(0, i, headers[i], cell_format)
-        row = 1
-        for connection_dict in connection_dicts:
-            cell_format = workbook.add_format()
-            cell_format.set_text_wrap()
-            highlight_green = False
-            highlight_blue = False
-            for connection_type in ['Inbound', 'Both']:
-                if connection_type in list(connection_dict.values()) and connection_dict['Arrival Time'] < '12:00':
-                    highlight_green = True
-            for connection_type in ['Outbound', 'Both']:
-                if connection_type in list(connection_dict.values()) and connection_dict['Departure Time'] >= '12:00':
-                    highlight_green = True
-            if 'Corridor' in list(connection_dict.values()):
-                highlight_blue = True
-            if highlight_green:
-                cell_format.set_bg_color('#6afc9f')
-            if highlight_blue:
-                cell_format.set_bg_color('#6bd7ff')
-            for k, v in connection_dict.items():
-                worksheet.write(row, headers.index(k), v, cell_format)
-            row += 1
-    workbook.close()
 
 # each column is agency, route, pattern, stop; eachrow is an arrival/departure
 # advantage is that it's much easier to filter in excel for stops with
 # many different routes
-def output_workbook_rows(workbook_name, connections):
-    workbook = xlsxwriter.Workbook('./output/rows/'+workbook_name+'.xlsx')
+def output_workbook(connections):
+    workbook = xlsxwriter.Workbook('./output/transit_connections.xlsx')
     cell_format = workbook.add_format()
     cell_format.set_text_wrap()
     for (headers, connection_dicts, station_name) in connections:
-        headers = ['Arrival Time', 'Departure Time', 'Connection', 'Agency', 'Route', 'Direction', 'Stop']
+        headers = ['Arrival Time', 'Departure Time', 'Connection', 'Agency', 'Route', 'Direction', 'Stop', 'Peak Connection']
         worksheet = workbook.add_worksheet(name=station_name)
         worksheet.autofilter(0, 0, 0, len(headers)-1)
         worksheet.set_column(0, 4, 15)
@@ -398,6 +385,7 @@ def output_workbook_rows(workbook_name, connections):
         for connection_dict in connection_dicts:
             cell_format = workbook.add_format()
             cell_format.set_text_wrap()
+            # TODO - this is so messy pls fix
             inbound = False
             outbound = False
             both = False
@@ -425,12 +413,11 @@ def output_workbook_rows(workbook_name, connections):
                 cell_format.set_bg_color('#6bd7ff')
             for k, v in connection_dict.items():
                 if k == 'Arrival Time':
-                   worksheet.write(row, 0, v, cell_format)
-                   continue
+                    worksheet.write(row, 0, v, cell_format)
+                    continue
                 if k == 'Departure Time':
                     worksheet.write(row, 1, v, cell_format)
                     continue
-                print(k)
                 sections = k.split('&gtfstoken&')
                 agency = sections[0].strip()
                 route = sections[1].strip()
@@ -445,54 +432,35 @@ def output_workbook_rows(workbook_name, connections):
                 elif outbound:
                     worksheet.write(row, 2, 'Outbound', cell_format)
                 else:
-                    worksheet.write(row, 2, 'None', cell_format)
+                    worksheet.write(row, 2, connection, cell_format)
                 worksheet.write(row, 3, agency, cell_format)
                 worksheet.write(row, 4, route, cell_format)
                 worksheet.write(row, 5, direction, cell_format)
                 worksheet.write(row, 6, stop, cell_format)
+                worksheet.write(row, 7, 'TRUE' if highlight_green else 'FALSE')
             row += 1
     workbook.close()
 
 
+input_dict = read_config()
+stations = read_stations(input_dict['input_path'])
+location_overrides = read_location_overrides(input_dict['input_path'])
 initialize_feeds()
-# read input.csv
-with open('input.csv', newline='') as csvfile:
-    inputreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    use_inputs_dir = next(inputreader)[1]
-    if use_inputs_dir != 'TRUE':
-        input_dict = read_input_file(inputreader)
-        connections = [get_local_msp_connections(
+station_connections = []
+for (station_name, corridors) in stations.items():
+    if station_name != '':
+        connections = get_local_msp_connections(
             station_name=station_name,
-            corridor_route_ids=input_dict['corridor_route_ids'],
+            corridor_route_ids=corridors,
             connection_max_distance=input_dict['connection_max_distance'],
             min_inbound_minutes=input_dict['min_inbound_minutes'],
             max_inbound_minutes=input_dict['max_inbound_minutes'],
             min_outbound_minutes=input_dict['min_outbound_minutes'],
             max_outbound_minutes=input_dict['max_outbound_minutes'],
-        ) for station_name in input_dict['station_names']]
-        # write each connection_df as an excel sheet in a workbook
-        # output_workbook('msp_connections', connections)
-        output_workbook_rows('msp_connections', connections)
-
-if use_inputs_dir == 'TRUE':
-    input_dir = 'inputs'
-    for input_file in os.listdir(input_dir):
-        with open(input_dir+'/'+input_file, newline='') as csvfile:
-            if '.csv' not in input_file:
-                continue
-            inputreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            input_dict = read_input_file(inputreader)
-            workbook_name = input_file.split('.csv')[0]
-            connections = [get_local_msp_connections(
-                station_name=station_name,
-                corridor_route_ids=input_dict['corridor_route_ids'],
-                connection_max_distance=input_dict['connection_max_distance'],
-                min_inbound_minutes=input_dict['min_inbound_minutes'],
-                max_inbound_minutes=input_dict['max_inbound_minutes'],
-                min_outbound_minutes=input_dict['min_outbound_minutes'],
-                max_outbound_minutes=input_dict['max_outbound_minutes'],
-            ) for station_name in input_dict['station_names']]
-            # write each connection_df as an excel sheet in a workbook
-            # with the name input_file (with an xslx extension)
-            # output_workbook(workbook_name, connections)
-            output_workbook_rows(workbook_name, connections)
+            only_show_corridors=input_dict['only_show_corridors'],
+            hourly_summary=input_dict['hourly_summary'],
+            location_overrides=location_overrides.get(station_name, []),
+        )
+        station_connections.append(connections)
+# write each connection_df as an excel sheet in a workbook having all stations
+output_workbook(station_connections)
